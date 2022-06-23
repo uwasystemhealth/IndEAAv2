24d6c3e26e0f9cf9ce4015943a241ddd651d9941
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-06-23 20:16:45 +0800

    initial settings configuration

---
+++ backend/config/settings/ci.py
@@ -0,0 +1,5 @@
+from .base import *  # noqa
+
+DEBUG = True
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
---
+++ backend/config/settings/prod.py
@@ -0,0 +1,7 @@
+from .base import *  # noqa
+
+DEBUG = False
+ALLOWED_HOSTS = ['*']
+
+# Do not use Prefix on production
+EMAIL_SUBJECT_PREFIX = ''
