63bfd805535665d2a4bcf2d223b8204b34d6607f
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-06-23 20:41:16 +0800

    format linting

--- backend/status/tests.py
+++ backend/status/tests.py
@@ -5,24 +5,23 @@ from django.test import Client
 
 def test_local_version_endpoint_is_accessible_to_anyone(client: Client) -> None:
     """
-    GIVEN:  A user who try to check status Spontane
+    GIVEN:  A user who try to check status IndEAA
     WHEN:   A user call /v1/status
     THEN:   User receives a status code equal to 200
     AND:    The user receives the app name
     """
 
     # Call without auth
-    url = reverse('api-v1:status:status')
-    response = client.get(url, HTTP_CLIENT_SOURCE='Tests')
+    url = reverse("api-v1:status:status")
+    response = client.get(url, HTTP_CLIENT_SOURCE="Tests")
 
-    app_name = response.json()['data']['Application name']
-    local_version = response.json()['data']['Local version']
-    environment = response.json()['data']['Environment']
-    settings_module = response.json(
-    )['data']['Variables']['DJANGO_SETTINGS_MODULE']
-    user_sentry = response.json()['data']['Variables']['USE_SENTRY']
-    sentry_env = response.json()['data']['Variables']['SENTRY_ENV']
-    gitshorthash = response.json()['data']['Variables']['GITSHORTHASH']
+    app_name = response.json()["data"]["Application name"]
+    local_version = response.json()["data"]["Local version"]
+    environment = response.json()["data"]["Environment"]
+    settings_module = response.json()["data"]["Variables"]["DJANGO_SETTINGS_MODULE"]
+    user_sentry = response.json()["data"]["Variables"]["USE_SENTRY"]
+    sentry_env = response.json()["data"]["Variables"]["SENTRY_ENV"]
+    gitshorthash = response.json()["data"]["Variables"]["GITSHORTHASH"]
 
     assert response.status_code == 200
     assert app_name == settings.APP_NAME
