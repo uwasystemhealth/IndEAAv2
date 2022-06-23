e9fa701b3d04c3006c0c4136cfe5fa972276c826
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-06-23 21:21:11 +0800

    next config to lint every folder

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  eslint: {
+    dirs: ['.'], // Run ESLint on all folders within /src. Used by 'yarn lint' command
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
