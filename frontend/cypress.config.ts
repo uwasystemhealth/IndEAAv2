7ab4a6a2e77d4596068d5c936ab6727ee19037ad
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-06-23 21:23:42 +0800

    cypress and jest setup

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  env: {},
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
