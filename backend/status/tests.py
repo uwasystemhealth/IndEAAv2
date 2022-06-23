21d37596e8b8942130817559a2096424cbbd4ecd
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-06-23 20:29:12 +0800

    initial status checks

---
+++ backend/status/tests.py
@@ -0,0 +1,34 @@
+from django.conf import settings
+from django.urls import reverse
+from django.test import Client
+
+
+def test_local_version_endpoint_is_accessible_to_anyone(client: Client) -> None:
+    """
+    GIVEN:  A user who try to check status Spontane
+    WHEN:   A user call /v1/status
+    THEN:   User receives a status code equal to 200
+    AND:    The user receives the app name
+    """
+
+    # Call without auth
+    url = reverse('api-v1:status:status')
+    response = client.get(url, HTTP_CLIENT_SOURCE='Tests')
+
+    app_name = response.json()['data']['Application name']
+    local_version = response.json()['data']['Local version']
+    environment = response.json()['data']['Environment']
+    settings_module = response.json(
+    )['data']['Variables']['DJANGO_SETTINGS_MODULE']
+    user_sentry = response.json()['data']['Variables']['USE_SENTRY']
+    sentry_env = response.json()['data']['Variables']['SENTRY_ENV']
+    gitshorthash = response.json()['data']['Variables']['GITSHORTHASH']
+
+    assert response.status_code == 200
+    assert app_name == settings.APP_NAME
+    assert local_version == settings.APP_VER
+    assert environment == settings.APP_ENV
+    assert settings_module == settings.DJANGO_SETTINGS_MODULE
+    assert user_sentry == settings.USE_SENTRY
+    assert sentry_env == settings.SENTRY_ENV
+    assert gitshorthash == settings.GITSHORTHASH
