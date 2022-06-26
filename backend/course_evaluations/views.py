9e137dc6ba2474fea303b6ac88c806e3c13992e8
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-06-27 04:24:10 +0800

    flake8 fixes

--- backend/course_evaluations/views.py
+++ backend/course_evaluations/views.py
@@ -1,5 +1,4 @@
 from rest_framework import permissions, status, viewsets
-from rest_framework.decorators import action
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
