2c006191b125ed2ac9ed4729dcce07ee5ae486ce
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-06-27 03:22:30 +0800

    initialise permissions

---
+++ backend/course_evaluations/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+class IsCoordinatorAllowAll(permissions.BasePermission):
+    """
+    Custom permission to only allow coordinators the API
+    """
+
+    def has_object_permission(self, request, view, obj):
+        """
+        A coordinator should be allowed to perform any operation
+        """
+
+        return request.user in obj.coordinators.all()
