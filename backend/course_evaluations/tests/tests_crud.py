0da6d1542924a52a611d6109a88506c888e9d0a0
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-07-02 15:09:51 +0800

    test create course evaluation

--- backend/course_evaluations/tests/tests_crud.py
+++ backend/course_evaluations/tests/tests_crud.py
@@ -7,6 +7,8 @@ from django.conf import settings
 from django.urls import reverse
 from rest_framework import status
 
+from course_evaluations.models import CourseEvaluation, EOCSet
+
 
 def test_list_view_course_evaluation(api_client_with_credentials_return_user, make_course_evaluation):
     """
@@ -48,3 +50,28 @@ def test_list_view_course_evaluation(api_client_with_credentials_return_user, ma
     # Check that there are certain fields that does not exist
     assert "eoc_set" not in course_evaluation_from_endpoint
     assert "eoc_set_id" not in course_evaluation_from_endpoint
+
+
+def test_create_course_evaluation(setup_indeaa, api_client_with_credentials_return_user):
+    """
+    GIVEN: As a user of the system
+    WHEN: I create a course evaluation
+    THEN: The course evaluation is created successfully
+    """
+    api_client, user = api_client_with_credentials_return_user()
+
+    url = reverse("api-v1:course_evaluations:course-evaluations-list")
+    data = {
+        "eoc_set_id": str(EOCSet.objects.first().id),
+        "unit_code": "TEST1002",
+        "description": "Test Creation of CourseEvaluation",
+    }
+    response = api_client.post(url, data)
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+    # Check that the course evaluation is created
+    course_evaluation = CourseEvaluation.objects.first()
+    assert course_evaluation.unit_code == data["unit_code"]
+    assert course_evaluation.description == data["description"]
+    assert course_evaluation.eoc_set == EOCSet.objects.first()
