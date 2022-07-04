d355bbb3e570a75eff63ddd14efa0132a228ecc3
chayG5 <chaygango5@gmail.com>
2022-07-04 17:28:55 +0800

    background image and welcome box

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,16 +1,33 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider } from '@mui/material/styles';
+//import image from 'public/bg7.png';
 import Header from '@/components/utils/Header';
 import CustomTheme from '@/components/utils/CustomTheme';
 import { AppProvider } from '@/components/Context/TopLevelContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const myStyle={
+    backgroundImage: 
+"url('https://raw.githubusercontent.com/uwasystemhealth/IndEAA/develop/client/assets/img/bg7.jpg')",
+    height:'100vh',
+    marginTop:'-100px',
+    backgroundSize: 'cover',
+
+};
   return (
     <AppProvider>
       <ThemeProvider theme={CustomTheme}>
         <Header />
+        <div style={myStyle}>    
         <Component {...pageProps} />
+        <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}} className='rounded-lg bg-grey p-3 py-2 px-5 text-center shadow-lg sm:py-4'>
+        <div style={{backgroundColor: 'grey', textAlign: 'center', height: 200, width: 800}}>
+          <h1>Welcome</h1>
+          <p>Please login to select your role</p>
+        </div>
+        </div>
+        </div>
       </ThemeProvider>
     </AppProvider>
   );
