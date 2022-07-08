cf71f7784abe7f9f3a8038256a1738c9ad060151
Michael Nef <michael@nef.net.au>
2022-07-08 11:07:55 +0800

    reviewer page general structure

--- frontend/pages/reviewer.tsx
+++ frontend/pages/reviewer.tsx
@@ -1,4 +1,7 @@
+import { useState } from 'react'
+
 import type { NextPage } from 'next'
+
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -6,14 +9,18 @@ import styles from '../styles/Home.module.css'
 
 import BodyCard from '../components/utils/BodyCard'
 
-import { Card } from '@mui/material';
+import { Card, Button } from '@mui/material';
 
 const Reviewer: NextPage = () => {
+
+  const showArchived = useState(false);
+
   return (
     <div className={styles.container}>
-      <BodyCard header="swag">
+      <BodyCard header="Review Courses">
         <div>
-          swag
+          <Button>Archived</Button>
+          <OngoingReviewsList showArchived={showArchived} />
         </div>
       </BodyCard>
     </div>
