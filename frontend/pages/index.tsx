e3d7dd3ded212ee891b887ba2ddc7c3890b7a7ed
chayG5 <chaygango5@gmail.com>
2022-07-12 11:51:16 +0800

    if user authenticated then ask to pick role

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -14,6 +14,11 @@ import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
 import { API_ENDPOINT, DEFAULT_USER_API_RESPONSE, UserAPIResponse } from 'utils/api';
 import styles from '../styles/Home.module.css';
+import { useContext } from 'react';
+import { useRouter } from 'next/router';
+import AppContext from 'components/Context/TopLevelContext';
+import useAuthenticatedAPIClient from '@/components/hooks/useAuthenticatedAPIClient';
+import { determineIfUserIsAuthentication } from 'utils/Authentication';
 
 const Home: NextPage = () => {
   // Change these data when this issue gets worked on. For now it just contains a demo of how to get user info
@@ -31,31 +36,36 @@ const Home: NextPage = () => {
   const handleChange = (event: SelectChangeEvent) => {
     setRole(event.target.value as string);
   };
+  const router = useRouter();
+  const axios = useAuthenticatedAPIClient();
+  const { authenticationDetails, setAuthenticationDetails } = useContext(AppContext);
+  const isUserAuthenticated = determineIfUserIsAuthentication(authenticationDetails.accessToken);
 
   return (  
-    // <Grid
-    //   container
-    //   spacing={0}
-    //   direction="column"
-    //   alignItems="center"
-    //   justify="center"
-    //   style={{ minHeight: '100vh', paddingTop: '120px'}}
-    // >
-    //   <Card style={{opacity: 0.8, width: '70%'}}>
-    //     <Grid sx={{minWidth: 500, minHeight: 250}} container alignItems="center" justifyContent="center">
-    //     <CardContent>
-    //       <Typography sx={{ fontSize: 36 }} color="text.secondary" gutterBottom>
-    //         Welcome!
-    //       </Typography>
-    //       <Typography variant="body2">
-    //         Please Login to continue
-    //       </Typography>
-    //     </CardContent>
-    //     </Grid>
-    //   </Card>
-    // </Grid>
-
+    <div>
     <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justify="center"
+      style={{ minHeight: '100vh', paddingTop: '120px'}}
+    >
+      <Card style={{opacity: 0.8, width: '70%'}}>
+        <Grid sx={{minWidth: 500, minHeight: 250}} container alignItems="center" justifyContent="center">
+        <CardContent>
+          <Typography sx={{ fontSize: 36 }} color="text.secondary" gutterBottom>
+            Welcome!
+          </Typography>
+          <Typography variant="body2">
+            Please Login to continue
+          </Typography>
+        </CardContent>
+        </Grid>
+      </Card>
+    </Grid>
+
+    {isUserAuthenticated && <Grid
       container
       spacing={0}
       direction="column"
@@ -98,6 +108,8 @@ const Home: NextPage = () => {
         </Grid>
       </Card>
     </Grid>
+}
+    </div>
     
     
     // <div>
