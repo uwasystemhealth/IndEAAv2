45b8487447719ad35807aff6a7fbecb5e0c5d4fa
chayG5 <chaygango5@gmail.com>
2022-07-14 00:03:58 +0800

    course evaluation listing

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -12,6 +12,7 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select, { SelectChangeEvent } from '@mui/material/Select';
 import CardContent from '@mui/material/CardContent';
+import CardHeader from '@mui/material/CardHeader';
 import Typography from '@mui/material/Typography';
 import {
   API_ENDPOINT,
@@ -26,12 +27,13 @@ import { useRouter } from 'next/router';
 import AppContext from 'components/Context/TopLevelContext';
 import useAuthenticatedAPIClient from '@/components/hooks/useAuthenticatedAPIClient';
 import { determineIfUserIsAuthentication } from 'utils/Authentication';
+import Button from '@mui/material/Button';
 
 const Home: NextPage = () => {
   // Change these data when this issue gets worked on. For now it just contains a demo of how to get user info
   const { response, isLoading, error } = useSWRAuth(API_ENDPOINT.COURSE_EVALUATION.LIST);
-  const hold = ((response?.data as unknown) || []) as CourseEvaluationListEntry[];
-  // console.log(hold.)
+  const courseEvaluationListEntries = ((response?.data as unknown) ||
+    []) as CourseEvaluationListEntry[];
   const [role, setRole] = React.useState('');
 
   const handleChange = (event: SelectChangeEvent) => {
@@ -43,82 +45,70 @@ const Home: NextPage = () => {
   const isUserAuthenticated = determineIfUserIsAuthentication(authenticationDetails.accessToken);
 
   return (
-    <div>
-      {!isUserAuthenticated ? (
-        <Container>
-          <Card>
-            <CardContent
+    <Container>
+      <Card>
+        <CardContent
+          sx={{
+            minHeight: '50vh',
+          }}
+        >
+          {!isUserAuthenticated ? (
+            <Box
               sx={{
                 display: 'flex',
                 flexDirection: 'column',
                 justifyContent: 'center',
                 alignItems: 'center',
-                minHeight: '50vh',
               }}
             >
               <Typography sx={{ fontSize: 36 }} color="text.secondary" gutterBottom>
                 Welcome!
               </Typography>
               <Typography variant="body2">Please Login to continue</Typography>
-            </CardContent>
-          </Card>
-        </Container>
-      ) : (
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justify="center"
-          style={{ minHeight: '100vh', paddingTop: '120px' }}
-        >
-          <Card style={{ width: '70%' }}>
-            <Grid
-              sx={{ minWidth: 500, maxHeight: 70, backgroundColor: 'darkOrange' }}
-              container
-              alignItems="center"
-              justifyContent="center"
+            </Box>
+          ) : (
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 2,
+              }}
             >
-              <CardContent>
-                <Typography sx={{ fontSize: 36 }} color="text.secondary" gutterBottom>
-                  Welcome!
-                </Typography>
-              </CardContent>
-            </Grid>
-          </Card>
-          <Card style={{ opacity: 0.8, width: '70%' }}>
-            <Grid
-              sx={{ minWidth: 500, minHeight: 250 }}
-              container
-              alignItems="center"
-              justifyContent="center"
-            >
-              <CardContent>
-                <Typography sx={{ fontSize: 24 }} variant="body2">
-                  Please Choose your Role
-                </Typography>
-                <br></br>
-                <Box sx={{ minWidth: 120 }}>
-                  <FormControl fullWidth>
-                    <InputLabel id="demo-simple-select-label">Role</InputLabel>
-                    <Select
-                      labelId="demo-simple-select-label"
-                      id="demo-simple-select"
-                      value={role}
-                      label="Role"
-                      onChange={handleChange}
+              {courseEvaluationListEntries.map((courseEvaluationListEntry) => (
+                <Card key={courseEvaluationListEntry.id}>
+                  <CardContent>
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                      }}
                     >
-                      <MenuItem value={10}>Coordinator</MenuItem>
-                      <MenuItem value={20}>Reviewer</MenuItem>
-                    </Select>
-                  </FormControl>
-                </Box>
-              </CardContent>
-            </Grid>
-          </Card>
-        </Grid>
-      )}
-    </div>
+                      <Box>
+                        <Typography gutterBottom variant="h5" component="div">
+                          {courseEvaluationListEntry.unit_code}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                          {courseEvaluationListEntry.description}
+                        </Typography>
+                      </Box>
+                      <Box
+                        sx={{
+                          display: 'flex',
+                          alignItems: 'center',
+                        }}
+                      >
+                        <Button>View</Button>
+                      </Box>
+                    </Box>
+                  </CardContent>
+                </Card>
+              ))}
+            </Box>
+          )}
+        </CardContent>
+      </Card>
+    </Container>
   );
 };
 
