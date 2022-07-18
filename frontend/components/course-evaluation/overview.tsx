4f7e09ce421a47e5596da2c6e5781defb2794cc2
chayG5 <chaygango5@gmail.com>
2022-07-18 17:20:51 +0800

    information section and buttons section done

--- frontend/components/course-evaluation/overview.tsx
+++ frontend/components/course-evaluation/overview.tsx
@@ -1,59 +1,74 @@
 import * as React from 'react';
-import { useSWRAuth } from '@/components/hooks/useSWRAuth';
 import type { NextPage } from 'next';
-import Tabs from '@mui/material/Tabs';
-import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { API_ENDPOINT, DEFAULT_USER_API_RESPONSE, UserAPIResponse } from 'utils/api';
-import styles from '../styles/Home.module.css';
-import { useContext } from 'react';
-import { useRouter } from 'next/router';
-import AppContext from 'components/Context/TopLevelContext';
-import useAuthenticatedAPIClient from '@/components/hooks/useAuthenticatedAPIClient';
-import { determineIfUserIsAuthentication } from 'utils/Authentication';
-import { Container } from '@mui/material';
+import { Button, Container, Stack } from '@mui/material';
 
-const Overview: NextPage = () => {
-  // Change these data when this issue gets worked on. For now it just contains a demo of how to get user info
-  const { response, isLoading, error } = useSWRAuth(API_ENDPOINT.AUTHENTICATION.USER);
-  const {
-    pk,
-    username,
-    email,
-    first_name: firstName,
-    last_name: lastName,
-  } = (response?.data as unknown as UserAPIResponse) || DEFAULT_USER_API_RESPONSE;
-
-  const router = useRouter();
-  const axios = useAuthenticatedAPIClient();
-  const { authenticationDetails, setAuthenticationDetails } = useContext(AppContext);
-  const isUserAuthenticated = determineIfUserIsAuthentication(authenticationDetails.accessToken);
-
-  const [value, setValue] = React.useState(0);
-
-  const handleChangeTab = (event: React.SyntheticEvent, newValue: number) => {
-    setValue(newValue);
-  };
-
-  return (
+const Overview: NextPage = () => {  
+    return (
     <Container>
       <Grid container spacing={5}>
         <Grid item xs={12} sm={6}>
-          <Card sx={{ minHeight: 400 }}>Information</Card>
+          <Card sx={{ maxWidth: 300, backgroundColor: '#0E91AC', color: 'white', p: 1 }}>
+            <Typography variant="subtitle2">INFORMATION</Typography>
+          </Card>
+          <Card sx={{ height: 400, overflow: 'auto' }}>
+            <Typography sx={{ p: 1, fontWeight: 'bold' }}>
+              Review Target Due Date:
+              <Typography variant="subtitle2">24th july 2022</Typography>
+            </Typography>
+            <Typography sx={{ p: 1, fontWeight: 'bold' }}>
+              Course Description:
+              <Typography variant="subtitle2">
+                qwertyudsgfvemhjblvjisrhnvlijrsvnfdcdkmvbrjlgbrjvbdjlvhnsrljhgrvnfjk-view-general-information-page
+                2df26e4 fixed code4 files changed, 151 insertionsrename frontend/componentsdelete
+                mode 100644 frontend/pages/coordinator-general.tsxcreate mode 100644 fronten
+              </Typography>
+            </Typography>
+            <Typography sx={{ p: 1, fontWeight: 'bold' }}>
+              Coordinators:
+              <Typography variant="subtitle2">
+                <List sx={{ listStyleType: 'disc' }}>
+                  <ListItem>Frinze</ListItem>
+                  <ListItem>Michael</ListItem>
+                </List>{' '}
+              </Typography>
+            </Typography>
+            <Typography sx={{ p: 1, fontWeight: 'bold' }}>
+              Date Started:
+              <Typography variant="subtitle2">10th july 2022</Typography>
+            </Typography>
+          </Card>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Card sx={{ minHeight: 400 }}>Documents</Card>
+          <Card sx={{ maxWidth: 300, backgroundColor: '#0E91AC', color: 'white', p: 1 }}>
+            <Typography variant="subtitle2">DOCUMENTS</Typography>
+          </Card>
+          <Card sx={{ minHeight: 400, overflow: 'auto' }}></Card>
         </Grid>
       </Grid>
       <Box sx={{ p: 2 }}>
+        <Card sx={{ maxWidth: 300, backgroundColor: '#0E91AC', color: 'white', p: 1 }}>
+          <Typography variant="subtitle2">QUICK ACTIONS</Typography>
+        </Card>
         <Card>
           <CardContent>
-            <Typography>Buttons</Typography>
+            <Stack spacing={5} direction="row">
+              <Button variant="contained" sx={{ backgroundColor: '#00BCD4' }}>
+                MARK AS COMPLETED
+              </Button>
+              <Button variant="contained" sx={{ backgroundColor: '#00BCD4' }}>
+                EDIT EVALUATION
+              </Button>
+              <Button variant="contained" sx={{ backgroundColor: '#00BCD4' }}>
+                MANAGE REVIEWERS
+              </Button>
+            </Stack>
           </CardContent>
         </Card>
       </Box>
