a585a1fc4dab67cad8ab9be2a3d671174a756c3e
Michael Nef <michael@nef.net.au>
2022-07-19 16:23:37 +0800

    added document reviews and eoc specific reviews

--- backend/reviews/views.py
+++ backend/reviews/views.py
@@ -2,8 +2,8 @@ from rest_framework import permissions, status, viewsets
 from rest_framework.exceptions import ValidationError
 from rest_framework.response import Response
 
-from reviews.models import Review
-from reviews.serializers import ReviewSerializer
+from reviews.models import Review, ReviewDocument, ReviewEocSpecific
+from reviews.serializers import ReviewSerializer, ReviewDocumentSerializer, ReviewEOCSpecificSerializer
 
 # Create your views here.
 
@@ -15,3 +15,24 @@ class ReviewsViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     def get_serializer(self, *args, **kwargs):
         return ReviewSerializer(*args, **kwargs)
+
+
+class ReviewDocumentViewSet(viewsets.ModelViewSet):
+    """
+    Viewset that handles the following
+    """
+
+    queryset = ReviewDocument.objects.all()
+    def get_serializer(self, *args, **kwargs):
+        return ReviewDocumentSerializer(*args, **kwargs)
+
+
+class ReviewEocSpecificViewSet(viewsets.ModelViewSet):
+    """
+    Viewset that handles the following
+    """
+
+    queryset = ReviewEocSpecific.objects.all()
+    def get_serializer(self, *args, **kwargs):
+        return ReviewEOCSpecificSerializer(*args, **kwargs)
+
