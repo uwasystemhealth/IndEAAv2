8019aae93ee8669cf49fed4c65586f90fc7a062a
Michael Nef <michael@nef.net.au>
2022-07-19 12:08:31 +0800

    moved reviews model to new app

--- backend/reviews/admin.py
+++ backend/reviews/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from reviews.models import Review
 
 # Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("id", "course_evaluation", "final_comment", "date_submitted")
+    ordering = ("created_at",)
--- backend/reviews/views.py
+++ backend/reviews/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import permissions, status, viewsets
+from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
+
+from reviews.models import Review
+from reviews.serializers import ReviewSerializer
 
 # Create your views here.
+
+class ReviewsViewSet(viewsets.ModelViewSet):
+    """
+    Viewset that handles the following
+    """
+
+    queryset = Review.objects.all()
+    def get_serializer(self, *args, **kwargs):
+        return ReviewSerializer(*args, **kwargs)
