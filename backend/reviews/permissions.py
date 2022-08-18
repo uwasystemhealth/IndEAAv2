252e202da624dbf27d7aca4e62da38e41ca4ebd3
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-08-18 13:31:49 +0800

    initiialise permission of a reviewer

--- backend/reviews/permissions.py
+++ backend/reviews/permissions.py
@@ -4,18 +4,17 @@ from rest_framework import permissions
 class IsReviewOwnerAllOrCoordinatorReadOnly(permissions.BasePermission):
     """
     Usecase:
-    - As a coordinator, I would like to create a Course Review (to indicate that someone is appointed as a reviewer)
+    - As a coordinator, I would like to create/delete a Course Review (to indicate that someone is appointed/removed as a reviewer)
+    - As a coordinator, I would like to open the submission
     - As a reviewer (owner), I will be able to update my review
     - As a review (not owner), I am not able to do anything
     - As anonymous, I am not able to do anything
     """
 
     def has_object_permission(self, request, view, obj):
-        if request.method in ["POST", "DELETE"]:
-            # Check if coordinator
-            if request.user in obj.course_evaluation.coordinators.all():
-                return True
-        else:
-            # Reviewer in this context is the associated reviewer in the object
-            return request.user == obj.reviewer
-        
+        # A coordinator can do anything
+        if request.user in obj.course_evaluation.coordinators.all():
+            return True
+
+        # Reviewer in this context is the associated reviewer in the object
+        return request.user == obj.reviewer
