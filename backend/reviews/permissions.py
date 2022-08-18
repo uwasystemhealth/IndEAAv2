cc6f75f0b83999b47d742b2a71779a0e9535e7fe
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-08-18 12:33:27 +0800

    permission for a course review

--- backend/reviews/permissions.py
+++ backend/reviews/permissions.py
@@ -3,14 +3,19 @@ from rest_framework import permissions
 
 class IsReviewOwnerAllOrCoordinatorReadOnly(permissions.BasePermission):
     """
-    Custom permission to only allow owners of an object to edit it or read only by coordinators.
-    Note: This is a modified version of https://www.django-rest-framework.org/tutorial/4-authentication-and-permissions/#object-level-permissions
+    Usecase:
+    - As a coordinator, I would like to create a Course Review (to indicate that someone is appointed as a reviewer)
+    - As a reviewer (owner), I will be able to update my review
+    - As a review (not owner), I am not able to do anything
+    - As anonymous, I am not able to do anything
     """
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if request.method is "POST":
+            # Check if coordinator
             if request.user in obj.course_evaluation.coordinators.all():
                 return True
-            
-        # Write permissions only for the owner
-        return obj.reviewer == request.user
+        else:
+            # Reviewer in this context is the associated reviewer in the object
+            return request.user == obj.reviewer
+        
