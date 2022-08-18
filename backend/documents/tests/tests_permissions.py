b7e8ce465a90c239d95e7cc652b45c5789bbd579
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-08-18 12:18:16 +0800

    initialise test permissions

---
+++ backend/documents/tests/tests_permissions.py
@@ -0,0 +1,165 @@
+"""
+This test file is focused on permission testing. This is to ensure that unauthorised access cannot use the API
+"""
+import pytest
+from django.urls import reverse
+from rest_framework import status
+
+from course_evaluations.models import CourseEvaluation
+from documents.models import Document
+
+
+@pytest.mark.django_db
+def test_list_view_course_evaluation_documents_anonymous(api_client_no_auth, make_course_evaluation):
+    """
+    GIVEN: The user is not authenticated
+    WHEN: the user tries to use the endpoint
+    THEN: The user is not authorised to use the endpoint
+    """
+    course_evaluation = make_course_evaluation(coordinators=[])
+    url = reverse(
+        "api-v1:course_evaluations:documents-list",
+        kwargs={"course_evaluation_id": course_evaluation.id},
+    )
+    response = api_client_no_auth.get(url)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.django_db
+def test_create_view_course_evaluation_documents_anonymous(api_client_no_auth, make_course_evaluation):
+    """
+    GIVEN: The user is not authenticated
+    WHEN: I create a course evaluation document
+    THEN:  The user is not authorised to use the endpoint
+    """
+    course_evaluation = make_course_evaluation(coordinators=[])
+    url = reverse(
+        "api-v1:course_evaluations:documents-list",
+        kwargs={"course_evaluation_id": course_evaluation.id},
+    )
+    data = {
+        #    Data does not matter as the user is not authenticated (the return error is 400 if data is invalid)
+    }
+    response = api_client_no_auth.post(url, data)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert CourseEvaluation.objects.count() == 1
+    assert Document.objects.count() == 0
+
+
+@pytest.mark.django_db
+def test_update_view_course_evaluation_documents_anonymous(api_client_no_auth, make_course_evaluation_document, make_course_evaluation):
+    """
+    GIVEN: The user is not authenticated
+    WHEN: I update a course evaluation document
+    THEN: The user is not authorised to use the endpoint
+    """
+    course_evaluation = make_course_evaluation()
+    document = make_course_evaluation_document(course_evaluation=course_evaluation)
+
+    url = reverse(
+        "api-v1:course_evaluations:documents-detail",
+        kwargs={"course_evaluation_id": course_evaluation.id, "pk": document.id},
+    )
+    data = {"name": "new name"}
+    response = api_client_no_auth.put(url, data)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert CourseEvaluation.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_delete_view_course_evaluation_documents_anonymous(api_client_no_auth, make_course_evaluation_document, make_course_evaluation):
+    """
+    GIVEN: The user is not authenticated
+    WHEN: I delete a course evaluation document
+    THEN: The user is not authorised to use the endpoint
+    """
+    course_evaluation = make_course_evaluation()
+
+    document = make_course_evaluation_document(course_evaluation=course_evaluation)
+
+    url = reverse(
+        "api-v1:course_evaluations:documents-detail",
+        kwargs={"course_evaluation_id": course_evaluation.id, "pk": document.id},
+    )
+    response = api_client_no_auth.delete(url)
+
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+    assert CourseEvaluation.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_create_view_course_evaluation_documents_reviewer(api_client_with_credentials_return_user, make_course_evaluation, make_course_review):
+    """
+    GIVEN: The user is authenticated and is a reviewer
+    WHEN: I create a course evaluation document
+    THEN:  The user is not authorised to use the endpoint
+    """
+    course_evaluation = make_course_evaluation(coordinators=[])
+    api_client, user = api_client_with_credentials_return_user()
+    make_course_review(course_evaluation=course_evaluation, reviewer=user)
+    url = reverse(
+        "api-v1:course_evaluations:documents-list",
+        kwargs={"course_evaluation_id": course_evaluation.id},
+    )
+    data = {
+        #    Data does not matter as The user is authenticated and is a reviewer (the return error is 400 if data is invalid)
+    }
+    response = api_client.post(url, data)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert CourseEvaluation.objects.count() == 1
+    assert Document.objects.count() == 0
+
+
+@pytest.mark.django_db
+def test_update_view_course_evaluation_documents_reviewer(
+    api_client_with_credentials_return_user, make_course_evaluation_document, make_course_evaluation, make_course_review
+):
+    """
+    GIVEN: The user is authenticated and is a reviewer
+    WHEN: I update a course evaluation document
+    THEN: The user is not authorised to use the endpoint
+    """
+    course_evaluation = make_course_evaluation()
+    api_client, user = api_client_with_credentials_return_user()
+    make_course_review(course_evaluation=course_evaluation, reviewer=user)
+
+    document = make_course_evaluation_document(course_evaluation=course_evaluation)
+
+    url = reverse(
+        "api-v1:course_evaluations:documents-detail",
+        kwargs={"course_evaluation_id": course_evaluation.id, "pk": document.id},
+    )
+    data = {"name": "new name"}
+    response = api_client.put(url, data)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert CourseEvaluation.objects.count() == 1
+
+
+@pytest.mark.django_db
+def test_delete_view_course_evaluation_documents_reviewer(
+    api_client_with_credentials_return_user, make_course_evaluation_document, make_course_evaluation, make_course_review
+):
+    """
+    GIVEN: The user is authenticated and is a reviewer
+    WHEN: I delete a course evaluation document
+    THEN: The user is not authorised to use the endpoint
+    """
+    course_evaluation = make_course_evaluation()
+    api_client, user = api_client_with_credentials_return_user()
+    make_course_review(course_evaluation=course_evaluation, reviewer=user)
+
+    document = make_course_evaluation_document(course_evaluation=course_evaluation)
+
+    url = reverse(
+        "api-v1:course_evaluations:documents-detail",
+        kwargs={"course_evaluation_id": course_evaluation.id, "pk": document.id},
+    )
+    response = api_client.delete(url)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+    assert CourseEvaluation.objects.count() == 1
