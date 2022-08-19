11304e02b961dabe4d187f732243cc4912052cab
Frinze Erin Lapuz <44391389+frinzekt@users.noreply.github.com>
2022-08-19 11:29:40 +0800

    4 Hotjar NextJS Implementation (#53)
    * specify environment
    
    * implementation of hotjar in codebase
    
    * documentation for telemetry and observability

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -4,7 +4,9 @@ import { ThemeProvider } from '@mui/material/styles';
 import image from 'public/bg7.png';
 import Header from '@/components/utils/Header';
 import CustomTheme from '@/components/utils/CustomTheme';
+import { useEffect } from 'react';
 import { AppProvider } from '@/components/Context/TopLevelContext';
+import { hotjar } from 'react-hotjar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const myStyle = {
@@ -12,6 +14,14 @@ function MyApp({ Component, pageProps }: AppProps) {
     minHeight: '100vh',
     backgroundSize: 'cover',
   };
+
+  useEffect(() => {
+    hotjar.initialize(
+      parseInt(process.env.NEXT_PUBLIC_HOTJAR_ID || '', 10),
+      parseInt(process.env.NEXT_PUBLIC_HOTJAR_SFV || '', 10),
+    );
+  }, []);
+
   return (
     <AppProvider>
       <ThemeProvider theme={CustomTheme}>
