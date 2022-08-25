1c2a81ada3ff36764a7e25c6def83d758bc718f3
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-08-25 10:42:15 +0800

    initialise review listing

--- frontend/components/CourseListings/Listings.tsx
+++ frontend/components/CourseListings/Listings.tsx
@@ -3,25 +3,26 @@ import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import { API_ENDPOINT, CourseEvaluationListEntry } from 'utils/api';
+import { API_ENDPOINT, CourseEvaluationListEntry, ReviewListEntry } from 'utils/api';
 import useSWRAuth from '@/components/hooks/useSWRAuth';
 import CoordinatorList from './CoordinatorList';
 import CustomTheme from '../utils/CustomTheme';
 import TabPanel, { a11yProps } from '../Custom/TabPanel';
+import ReviewList from './ReviewList';
 
 const Listings = () => {
   const [tabsValue, setTabsValue] = React.useState(0);
 
-  const { response } = useSWRAuth(API_ENDPOINT.COURSE_EVALUATION.LIST);
-  const courseEvaluationListEntries = ((response?.data as unknown) ||
+  const { response: courseEvaluationResponse } = useSWRAuth(API_ENDPOINT.COURSE_EVALUATION.LIST);
+  const courseEvaluationListEntries = ((courseEvaluationResponse?.data?.results as unknown) ||
     []) as CourseEvaluationListEntry[];
+  const { response: reviewsResponse } = useSWRAuth(API_ENDPOINT.REVIEWS.LIST);
+  const reviewListEntries = ((reviewsResponse?.data?.results as unknown) ||
+    []) as ReviewListEntry[];
 
   const handleChangeTab = (event: React.SyntheticEvent, newValue: number) => {
     setTabsValue(newValue);
   };
-
-  // under the review list branch this should be changed to something similar to courseEvaluationListEntries
-  const reviews: string[] = [];
 
   return (
     <Container
@@ -39,13 +40,13 @@ const Listings = () => {
             <Tab label="COURSE EVALUATIONS" {...a11yProps(0)} />
           )}
           {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-          {reviews.length > 0 && <Tab label="REVIEW COURSES" {...a11yProps(1)} />}
+          {reviewListEntries.length > 0 && <Tab label="REVIEW COURSES" {...a11yProps(1)} />}
         </Tabs>
         <TabPanel value={tabsValue} index={0}>
           <CoordinatorList list={courseEvaluationListEntries} />
         </TabPanel>
         <TabPanel value={tabsValue} index={1}>
-          Item Two
+          <ReviewList list={reviewListEntries} />
         </TabPanel>
       </Box>
     </Container>
---
+++ frontend/components/CourseListings/ReviewList.tsx
@@ -0,0 +1,92 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Container from '@mui/material/Container';
+import CardContent from '@mui/material/CardContent';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import Link from '@mui/material/Link';
+import { ReviewListEntry } from 'utils/api';
+
+type Props = {
+  list: ReviewListEntry[];
+};
+
+const ReviewList = ({ list }: Props) => (
+  <Container>
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'flex-end',
+        pb: 2,
+      }}
+    >
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          CREATE NEW EVALUATION
+        </Button>
+        <Button variant="contained" color="secondary">
+          SHOW COMPLETED
+        </Button>
+      </Stack>
+    </Box>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 2,
+      }}
+    >
+      {list.map((reviewEntry) => (
+        <Card key={reviewEntry.id}>
+          <CardContent>
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                justifyContent: 'space-between',
+              }}
+            >
+              <Box>
+                <Typography gutterBottom variant="h5" component="div">
+                  {reviewEntry.course_evaluation.unit_code}
+                </Typography>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                  }}
+                >
+                  <Typography sx={{ fontWeight: 'bold', pb: 1, pr: 1 }}>Coordinators:</Typography>
+                  <Typography>
+                    {reviewEntry.course_evaluation.coordinators
+                      .map(({ username }) => username)
+                      .join(', ')}
+                  </Typography>
+                </Box>
+                <Typography variant="body2" color="text.secondary">
+                  {reviewEntry.course_evaluation.description}
+                </Typography>
+              </Box>
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                }}
+              >
+                <Link href={`/course-evaluation/${reviewEntry.id}`}>
+                  <Button variant="contained" color="primary">
+                    View
+                  </Button>
+                </Link>
+              </Box>
+            </Box>
+          </CardContent>
+        </Card>
+      ))}
+    </Box>
+  </Container>
+);
+
+export default ReviewList;
