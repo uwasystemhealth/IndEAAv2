e357ccff4122aa3fe1e78e8233323b4f3fc30e05
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-08-25 11:28:43 +0800

    utility for determining state of review

---
+++ frontend/components/utils/reviews.ts
@@ -0,0 +1,10 @@
+/* eslint-disable import/prefer-default-export */
+import { ReviewListEntry } from 'utils/api';
+
+
+export const determineStepsStateOfReview = (review: ReviewListEntry) => ({
+  step1: Boolean(review.eoc_date_viewed),
+  step2: review.documents.length > 0,
+  step3: review.eoc_specific_reviews.length > 0,
+  step4: Boolean(review.date_submitted),
+});
