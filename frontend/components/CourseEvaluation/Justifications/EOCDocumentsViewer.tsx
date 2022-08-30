3de1da9ebbf07718ea5001d6bd007e690f9432af
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-08-30 09:33:31 +0800

    document viewer with switcher

--- frontend/components/CourseEvaluation/Justifications/EOCDocumentsViewer.tsx
+++ frontend/components/CourseEvaluation/Justifications/EOCDocumentsViewer.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Document, EocGeneralEocSpecific, EocSetEocGeneral } from 'utils/api';
+import Stack from '@mui/material/Stack';
+import DocumentCard from '../Documents/DocumentCard';
+import TabPanel, { a11yProps } from '@/components/Custom/TabPanel';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
 
 type Props = {
   documents: Document[];
@@ -14,7 +19,43 @@ type Props = {
  * This component handles the logic to display the documents depending on whether "General" or "Specific" Documents are wanted
  */
 const EOCDocumentsViewer = (props: Props) => {
-  return <div>EOCDocumentsViewer</div>;
+  const { documents, eocGeneralToFilter, eocSpecificToFilter } = props;
+  const documentsMatchingEOCGeneral = documents.filter((document) =>
+    document.eoc_generals.map((obj) => obj.id).includes(eocGeneralToFilter.id),
+  );
+  const documentsMatchingEOCSpecific = documents.filter((document) =>
+    document.eoc_specifics.map((obj) => obj.id).includes(eocSpecificToFilter.id),
+  );
+
+  const [tabsValue, setTabsValue] = React.useState(0);
+  const handleChangeTab = (event: React.SyntheticEvent, newValue: number) => {
+    setTabsValue(newValue);
+  };
+
+  return (
+    <div>
+      <Tabs value={tabsValue} onChange={handleChangeTab} variant="fullWidth">
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Tab label="General Documents" {...a11yProps(0)} />
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        <Tab label="Specific Documents" {...a11yProps(1)} />
+      </Tabs>
+      <TabPanel value={tabsValue} index={0}>
+        <Stack spacing={2}>
+          {documentsMatchingEOCGeneral.map((document) => (
+            <DocumentCard document={document} isReadOnly={false} />
+          ))}
+        </Stack>
+      </TabPanel>
+      <TabPanel value={tabsValue} index={1}>
+        <Stack spacing={2}>
+          {documentsMatchingEOCSpecific.map((document) => (
+            <DocumentCard document={document} isReadOnly={false} />
+          ))}
+        </Stack>
+      </TabPanel>
+    </div>
+  );
 };
 
 export default EOCDocumentsViewer;
