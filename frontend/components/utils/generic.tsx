688cef7a716098e82dbd57a22f8f5126314ef4b9
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-08-30 13:59:58 +0800

    added new types

--- frontend/components/utils/generic.tsx
+++ frontend/components/utils/generic.tsx
@@ -1,10 +1,9 @@
 /* eslint-disable import/prefer-default-export */
-import { Coordinator, UserAPIResponse } from 'utils/api';
+import { User } from 'utils/api';
 
-export const userDisplayName = (user: UserAPIResponse | Coordinator) => {
+export const userDisplayName = (user: User) => {
   const { email, first_name: firstName, last_name: lastName } = user;
   return `${firstName} ${lastName} (${email})`;
 };
 
-export const listOfUserDisplayNames = (users: UserAPIResponse[] | Coordinator[]) =>
-  users.map(userDisplayName).join(', ');
+export const listOfUserDisplayNames = (users: User[]) => users.map(userDisplayName).join(', ');
