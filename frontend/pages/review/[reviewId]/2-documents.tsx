3d903b08d2530ad412ba6a2c1994661964dd5a63
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-08-31 15:26:28 +0800

    initialised grid for displaying documents

--- frontend/pages/review/[reviewId]/2-documents.tsx
+++ frontend/pages/review/[reviewId]/2-documents.tsx
@@ -1,15 +1,37 @@
 import useCourseReview from '@/components/hooks/useCourseReview';
 import AboutStepCard from '@/components/Reviewer/AboutStepCard';
+import ReviewerBottomNavigation from '@/components/Reviewer/ReviewerBottomNavigation';
 import ReviewProgress from '@/components/Reviewer/ReviewProgress';
 import BodyCard from '@/components/utils/BodyCard';
+import { getReviewStepsWithState } from '@/components/utils/reviews';
+import Grid from '@mui/material/Grid';
+import { CourseEvaluationDetailEntry } from 'utils/api';
+import useModal from '@/components/hooks/useModal';
+import DocumentCard from '@/components/CourseEvaluation/Documents/DocumentCard';
+import useCourseEvaluation from '@/components/hooks/useCourseEvaluation';
 
 const Documents = () => {
   const { courseReview } = useCourseReview();
+  const { courseEvaluation } = useCourseEvaluation(courseReview.course_evaluation.id);
+
+  const STEP_INDEX = 1;
+  const stepDetails = getReviewStepsWithState(courseReview)[STEP_INDEX];
 
   return (
     <BodyCard>
       <ReviewProgress review={courseReview} />
-      <AboutStepCard stepIndex={1} />
+      <AboutStepCard stepIndex={STEP_INDEX} />
+      <Grid container spacing={2}>
+        {courseEvaluation.documents.map((document) => (
+          <Grid item sm={12} md={4} key={document.id}>
+            <DocumentCard document={document} isReadOnly />
+          </Grid>
+        ))}
+      </Grid>
+      <ReviewerBottomNavigation
+        previousLink={stepDetails.prevStep}
+        nextLink={stepDetails.nextStep}
+      />
     </BodyCard>
   );
 };
