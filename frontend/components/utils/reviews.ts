ef41b1389a2e8ef9ee8e405ef7485d111ff84a88
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-08-31 11:46:58 +0800

    review steps with description

--- frontend/components/utils/reviews.ts
+++ frontend/components/utils/reviews.ts
@@ -7,22 +7,30 @@ export const getReviewStepsWithState = (review: ReviewListEntry) => {
       stepName: 'Overview & Elements of Competency',
       stepLink: 'overview-and-eoc',
       done: Boolean(review.eoc_date_viewed),
+      description:
+        'This section is an introduction of the course and the review process. Below you will see documents that introduces the unit, as well as the Elements of Competencies (EOCs) and their indicators of attainment.',
     },
     {
       stepName: 'Read Documents',
       stepLink: 'documents',
       done: review.documents.length > 0,
+      description:
+        'This section contains all the documents related in this review. You will see also in the assessment section when reviewing specific EOCs. Please Review the following documents. These should form the basis of your assessment of how this course contributes to the attainment of the Element of Competency outlined in step 1',
     },
 
     {
       stepName: 'Review Course',
       stepLink: 'assessment',
       done: review.eoc_specific_reviews.length > 0,
+      description:
+        'This section contains all the elements of competencies to be assessed. Please click the EOC below to rate and give comment.',
     },
     {
       stepName: 'Review & Submit',
       stepLink: 'submit',
       done: Boolean(review.date_submitted),
+      description:
+        'This section contains the summary of all your responses per section. Please review before submitting.',
     },
   ];
 
