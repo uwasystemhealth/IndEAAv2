3ddcdd777d185ed8a90cf90664017c54d4723c29
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-09-01 17:12:28 +0800

    displaying justification and suggestion of a reviewer

--- frontend/components/CourseEvaluation/Justifications/EOCCard.tsx
+++ frontend/components/CourseEvaluation/Justifications/EOCCard.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { EocGeneralEocSpecific } from 'utils/api';
+import { EocGeneralEocSpecific, ReviewListEntry } from 'utils/api';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
@@ -10,17 +10,34 @@ import Box from '@mui/material/Box';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import { Divider, useTheme } from '@mui/material';
 import { developmentLevelToString } from '@/components/utils/eoc';
+import { getReviewerAssessment } from '@/components/utils/reviews';
 
 type Props = {
   eocSpecific: EocGeneralEocSpecific;
   handleSelectEOCSpecific: (eocSpecific: EocGeneralEocSpecific) => void;
+
+  // This is useful for Reviewer
+  review?: ReviewListEntry;
 };
 
 const EOCCard = (props: Props) => {
-  const { eocSpecific, handleSelectEOCSpecific } = props;
+  const { eocSpecific, handleSelectEOCSpecific, review } = props;
   // Business rule: There can only be one justification per EOC
-  const justification = eocSpecific?.justification[0];
+  const coordinatorJustification = eocSpecific?.justification[0];
   const theme = useTheme();
+
+  const isReviewerDisplay = review;
+  const reviewerAssessment = isReviewerDisplay
+    ? getReviewerAssessment(review, eocSpecific)
+    : undefined;
+
+  const developmentLevel = isReviewerDisplay
+    ? reviewerAssessment?.development_level || 0
+    : coordinatorJustification.development_level || 0;
+
+  const justificationText = isReviewerDisplay
+    ? reviewerAssessment?.justification
+    : coordinatorJustification.justification;
 
   return (
     <Card>
@@ -53,36 +70,53 @@ const EOCCard = (props: Props) => {
         <Box>
           <Box
             sx={{
-              color: justification?.development_level
-                ? theme.palette.secondary.main
-                : theme.palette.error.main,
+              // Development level that have no selection is 0 by default
+              color: developmentLevel ? theme.palette.secondary.main : theme.palette.error.main,
             }}
           >
             <Typography variant="body2" sx={{ fontWeight: 'bold', pb: 1, pr: 1 }} color="success">
               Your Rating:
             </Typography>
             <Typography variant="body2" gutterBottom>
-              {developmentLevelToString[justification?.development_level || 0]}
+              {developmentLevelToString[developmentLevel]}
             </Typography>
           </Box>
           <Box
             sx={{
-              color: justification?.justification
-                ? theme.palette.secondary.main
-                : theme.palette.error.main,
+              color: justificationText ? theme.palette.secondary.main : theme.palette.error.main,
             }}
           >
             <Typography variant="body2" sx={{ fontWeight: 'bold', pb: 1, pr: 1 }} color="success">
               Your Justification:
             </Typography>
             <Typography variant="body2" gutterBottom>
-              {justification?.justification || 'None'}
+              {justificationText || 'None'}
             </Typography>
           </Box>
+          {isReviewerDisplay && (
+            <Box
+              sx={{
+                color: reviewerAssessment?.suggestion
+                  ? theme.palette.secondary.main
+                  : theme.palette.error.main,
+              }}
+            >
+              <Typography variant="body2" sx={{ fontWeight: 'bold', pb: 1, pr: 1 }} color="success">
+                Your Suggestion
+              </Typography>
+              <Typography variant="body2" gutterBottom>
+                {reviewerAssessment?.suggestion || 'None'}
+              </Typography>
+            </Box>
+          )}
         </Box>
       </CardContent>
     </Card>
   );
+};
+
+EOCCard.defaultProps = {
+  review: undefined,
 };
 
 export default EOCCard;
