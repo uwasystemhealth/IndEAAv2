dc703f1fe2720543e6327cb23c3045d70dc49733
Frinze Erin Lapuz <frinzekt@gmail.com>
2022-09-02 12:00:51 +0800

    refactor a the stepWrapper

--- frontend/pages/review/[reviewId]/4-submit.tsx
+++ frontend/pages/review/[reviewId]/4-submit.tsx
@@ -13,6 +13,19 @@ import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
 import DocumentCard from '@/components/CourseEvaluation/Documents/DocumentCard';
 import SimplifiedDocumentCard from '@/components/Reviewer/Submit/SimplifiedDocumentCard';
+import React, { ReactNode } from 'react';
+
+const StepWrapper = (props: { children: ReactNode; cardTitle: string }) => {
+  const { children, cardTitle } = props;
+  return (
+    <Container maxWidth="xl" sx={{ mt: 2, mb: 2 }}>
+      <Card>
+        <CardHeader title={cardTitle} />
+        <CardContent>{children}</CardContent>
+      </Card>
+    </Container>
+  );
+};
 
 const Submit = () => {
   const { courseReview } = useCourseReview();
@@ -27,41 +40,27 @@ const Submit = () => {
     <BodyCard>
       <ReviewProgress review={courseReview} />
       <AboutStepCard stepIndex={STEP_INDEX} />
-      <Container maxWidth="xl" sx={{ mt: 2, mb: 2 }}>
-        <Card>
-          <CardHeader title={`Step 1 - ${allSteps[0].stepName}`} />
-          <CardContent>
-            <Container maxWidth="xl">
-              {courseReview.eoc_date_viewed ? (
-                <Typography color="secondary.main">
-                  You have read and confirmed that you have understood the elements of competencies.
-                </Typography>
-              ) : (
-                <Typography color="error.main">
-                  You have not yet read and confirmed that you have understood the elements of
-                  competencies.
-                </Typography>
-              )}
-            </Container>
-          </CardContent>
-        </Card>
-      </Container>
-      <Container maxWidth="xl" sx={{ mt: 2, mb: 2 }}>
-        <Card>
-          <CardHeader title={`Step 2 - ${allSteps[1].stepName}`} />
-          <CardContent>
-            <Container maxWidth="xl">
-              <Grid container spacing={2}>
-                {courseEvaluation.documents.map((document) => (
-                  <Grid item sm={12} md={4} key={document.id}>
-                    <SimplifiedDocumentCard document={document} review={courseReview} />
-                  </Grid>
-                ))}
-              </Grid>
-            </Container>
-          </CardContent>
-        </Card>
-      </Container>
+      <StepWrapper cardTitle={`Step 1 - ${allSteps[0].stepName}`}>
+        {courseReview.eoc_date_viewed ? (
+          <Typography color="secondary.main">
+            You have read and confirmed that you have understood the elements of competencies.
+          </Typography>
+        ) : (
+          <Typography color="error.main">
+            You have not yet read and confirmed that you have understood the elements of
+            competencies.
+          </Typography>
+        )}
+      </StepWrapper>
+      <StepWrapper cardTitle={`Step 2 - ${allSteps[1].stepName}`}>
+        <Grid container spacing={2}>
+          {courseEvaluation.documents.map((document) => (
+            <Grid item sm={12} md={4} key={document.id}>
+              <SimplifiedDocumentCard document={document} review={courseReview} />
+            </Grid>
+          ))}
+        </Grid>
+      </StepWrapper>
       <ReviewerBottomNavigation
         previousLink={stepDetails.prevStep}
         nextLink={stepDetails.nextStep}
