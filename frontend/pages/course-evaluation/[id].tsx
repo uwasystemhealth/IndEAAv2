5fe7ed5275757282b4f8b5bb5c965f86549a24ad
Michael Nef <michael@nef.net.au>
2022-09-05 16:54:27 +0800

    added unit code to title for coordinator page

--- frontend/pages/course-evaluation/[id].tsx
+++ frontend/pages/course-evaluation/[id].tsx
@@ -1,10 +1,21 @@
+import { useEffect } from 'react';
 import SectionTabs from '@/components/CourseEvaluation/SectionTabs';
 import BodyCard from '@/components/utils/BodyCard';
+import useCourseEvaluation from '@/components/hooks/useCourseEvaluation';
 
-const Post = () => (
+const Post = () => {
+  
+  const { courseEvaluation } = useCourseEvaluation();
+    
+  // set document title to unit code
+  useEffect(() => {
+    document.title = courseEvaluation.unit_code + " Manage";
+  }, [courseEvaluation.unit_code]);
+
+  return (
   <BodyCard>
     <SectionTabs />
   </BodyCard>
-);
+)};
 
 export default Post;
