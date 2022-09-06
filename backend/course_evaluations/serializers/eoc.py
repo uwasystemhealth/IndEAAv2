86a5e6a77305e6f29fd7ef22b067d171c15aa3aa
Frinze Erin Lapuz <44391389+frinzekt@users.noreply.github.com>
2022-09-06 17:41:59 +0800

    114 bug course evaluation justification change on one course evaluation changes as well in another (#115)
    * passing of context to allow data passing through nesting
    
    * automated testing for the specific scenario
    
    * reposition comment

--- backend/course_evaluations/serializers/eoc.py
+++ backend/course_evaluations/serializers/eoc.py
@@ -10,7 +10,7 @@ class EOCSetSerializer(serializers.ModelSerializer):
     class EOCGeneralSerializer(serializers.ModelSerializer):
         class EOCSpecificSerializer(serializers.ModelSerializer):
             # Information to be known by both coordinator and reviewer
-            justification = CourseEvaluationJustificationSerializer(many=True, read_only=True)
+            justification = serializers.SerializerMethodField("get_justification_filtered_with_course_evaluation")
 
             class Meta:
                 model = EOCSpecific
@@ -23,6 +23,14 @@ class EOCSetSerializer(serializers.ModelSerializer):
                     "indicators_of_attainment",
                     "justification",
                 )
+
+            def get_justification_filtered_with_course_evaluation(self, instance):
+                """
+                Get the justification for this specific EOC with the course_evaluation_id in the context
+                """
+                course_evaluation_id = self.context.get("course_evaluation_id")
+                filtered_justifications = instance.justification.filter(course_evaluation_id=course_evaluation_id)
+                return CourseEvaluationJustificationSerializer(filtered_justifications, many=True).data
 
         eoc_specifics = EOCSpecificSerializer(many=True, read_only=True, source="eocspecific_set")
 
