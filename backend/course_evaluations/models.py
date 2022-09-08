9216eba06365f176cf770e33843f9bb5cfd0cec5
Chayanika Gangopadhyay <104259880+chayG5@users.noreply.github.com>
2022-09-08 14:31:29 +0800

    107 feature length of inputs (#116)
    * Reviewer document title now is course code
    
    * Always refresh document title whenever unit code changes
    
    * added unit code to title for coordinator page
    
    * make length of unit_code and document name to 100
    
    * fix build errors
    
    * fix build errors
    
    * fix build error
    
    * miration file
    
    Co-authored-by: Frinze Erin Lapuz <44391389+frinzekt@users.noreply.github.com>
    Co-authored-by: Michael Nef <michael@nef.net.au>

--- backend/course_evaluations/models.py
+++ backend/course_evaluations/models.py
@@ -66,7 +66,7 @@ class CourseEvaluation(models.Model):
 
     # Giving this char field a flexibility
     # Eg. CITS3403/CITS5505 (This is around 8+8+1 = 17 characters)
-    unit_code = models.CharField(max_length=20, null=False, blank=True)
+    unit_code = models.CharField(max_length=100, null=False, blank=True)
     description = models.TextField(null=False, blank=True)
 
     # Many-to-many Relationship with the Django User model
@@ -123,7 +123,7 @@ class Document(models.Model):
     course_evaluation = models.ForeignKey(CourseEvaluation, on_delete=models.CASCADE, related_name="documents")
 
     # Information about the document
-    name = models.CharField(max_length=50, null=False, blank=False)
+    name = models.CharField(max_length=100, null=False, blank=False)
     description = models.TextField(null=False, blank=True)
     url = models.URLField()
 
